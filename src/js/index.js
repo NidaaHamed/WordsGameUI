1349ee1afa44b63b6893ff546cc0ef90432ab6de
NidaaHamed <nidaasallam@gmail.com>
2024-03-31 20:39:08 +0200

    generate grid

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,15 @@
+import {Grid} from "./grid"
 const submitWordBtn = document.querySelector(".submit-word");
+const grid = new Grid();
+const GRID_SIZE = 10
 submitWordBtn.addEventListener("click",async ()=>{
     let result = await fetchGridInfo(["ONE","TWO","THREE"]);
-    console.log(result);
+    // console.log(result);
+    grid.renderGrid(GRID_SIZE,result);
 });
 async function fetchGridInfo(wordList){
     const commaSeparatedWords = wordList.join(",");
-    let response = await fetch(`http://localhost:8080/wordgrid?gridSize=10&wordsList=${commaSeparatedWords}`);
+    let response = await fetch(`http://localhost:8080/wordgrid?gridSize=${GRID_SIZE}&wordsList=${commaSeparatedWords}`);
     let result = await response.text();
-    return result;
+    return result.split(" ");
 }
